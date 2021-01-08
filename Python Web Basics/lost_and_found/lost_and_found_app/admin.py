a5c89c56659d76ad29553872b3c7fc4e1a432a9b
vjord-it <villyjord@yahoo.com>
2021-01-08 23:12:26 +0200

    removing copyrighted materials
    Since Sept 2020 all softuni materials have become copyrighted and cannot be reproduced. So I remove everything from Sept 2020 on except for the intro first lectures which have no essential educational content.

--- Python Web Basics/lost_and_found/lost_and_found_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from lost_and_found_app import models
-
-# Register your models here.
-admin.site.register(models.Post)
-admin.site.register(models.Object)
--- Python Web Basics/lost_and_found/lost_and_found_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LostAndFoundAppConfig(AppConfig):
-    name = 'lost_and_found_app'
